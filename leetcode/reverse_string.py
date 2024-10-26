bf7566e431d5decd5cace08b0a9ba092388d3930
nicholas <aigberuan6@gmail.com>
2024-10-26 06:14:10 +0100

    ed

--- leetcode/reverse_string.py
+++ leetcode/reverse_string.py
@@ -6,15 +6,3 @@ def string_rev():
 string_rev()
 
 
-def string_rev():
-    word = input('Enter your string: ')
-    reversed_word = ''
-    
-    # Loop from the last index to the first index
-    for i in range(len(word) - 1, -1, -1):
-        reversed_word += word[i]  # Add each character to the result string
-    
-    print(reversed_word)
-
-string_rev()
-
