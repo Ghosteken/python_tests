4ce7c044ef74a1e0048a35c8adf78e24e3b82743
nicholas <aigberuan6@gmail.com>
2024-11-06 13:19:53 +0100

    work

---
+++ leetcode/swap.py
@@ -0,0 +1,7 @@
+def swap():
+    a = int(input('what is a'))
+    b = int(input('what is b'))
+    [a,b] = [b,a]
+    print(a,b)  
+swap()        
+    
--- leetcode/vowel2.py
+++ leetcode/vowel2.py
@@ -2,9 +2,9 @@ def vcount():
     cats = input('Enter your string: ')
     vowels = ['a', 'e', 'i', 'o', 'u']
     found_vowels = ''
-    for i in cats.lower():  # Use .lower() for case-insensitivity
-        if i in vowels:  # Check if the character is a vowel
-            found_vowels += i  # Add the vowel to the found_vowels string
+    for i in cats.lower():
+        if i in vowels:  
+            found_vowels += i  
     print("Number of vowels:", len(found_vowels))
 
 vcount()
