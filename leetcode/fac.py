ff17578000bf97dce09c6e31bdaeeff685e7184f
nicholas <aigberuan6@gmail.com>
2024-11-18 13:23:13 +0100

    rec

---
+++ leetcode/fac.py
@@ -0,0 +1,16 @@
+def soln():
+    num = int(input("Enter a number: "))
+    
+    def factorial(n):
+        # Base case for recursion
+        if n == 0 or n == 1:
+            return 1
+        else:
+            # Recursive case
+            return n * factorial(n - 1)
+    
+    # Call the factorial function and print the result
+    result = factorial(num)
+    print(f"The factorial of {num} is {result}")
+
+            
